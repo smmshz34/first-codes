95075c222f4ee3f707f8c9e8b235da81d0c6782c
smmshz34 <69580504+smmshz34@users.noreply.github.com>
2020-12-18 19:09:52 +0330

    Create compare numbers in array.c

---
+++ compare numbers in array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void readnum(float* num,int n) {
+	for (int i = 0; i < n; i++)
+	{
+		printf("Enter number: ");
+		scanf_s("%f", &num[i]);
+	}
+}
+float compare(float* num,int n) {
+	float* max = NULL;
+	max = &num[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (*max>num[i])
+		{
+			*max = num[i];
+		}
+	}
+	return *max;
+}
+int main() {
+	int n;
+	float numbers[20], average = 0;
+	printf("enter number of numbers: ");
+	scanf_s("%d", &n);
+	readnum(&numbers, n);
+	printf("%f", compare(&numbers, n));
+}
